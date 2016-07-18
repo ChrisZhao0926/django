4f9f7568117db96a76d8de79dde04169fcbc0ed6
chris <chris.zhao@sonymobile.com>
2016-07-18 14:56:52 +0800

    modify POST and GET methed

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        # url(r'^sendmail/$', views.sendmail(), name='sendemail'),
                        url(r'^check/$', views.check_request, name='check'),
+                       url(r'^name/$', views.get_name, name='get_name'),
+                       url(r'^add/$', views.add, name='add'),
+                       url(r'^add_post/$', views.add_form, name='add'),
                        )
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.mail import EmailMessage
 import os
+from .forms import NameForm, AddForm
+from django.shortcuts import render
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 
 def index(request):
 
-    return HttpResponse("Hello world!")
+    return render(request, 'polls/add.html')
 
 
 def sendmail():
@@ -25,3 +27,36 @@ def sendmail():
 def check_request(request):
     my_path = request.path
     return HttpResponse(my_path)
+
+
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            #return HttpResponseRedirect('/thanks/')
+            return HttpResponse(form.cleaned_data.get('your_name'))
+    else:
+        form = NameForm()
+
+    return render(request, 'polls/name.html', {'form': form})
+
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    a1 = int(a)
+    b1 = int(b)
+    return HttpResponse(str(a1+b1))
+
+
+def add_form(request):
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        if form.is_valid():
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            return HttpResponse(str(int(a)+int(b)))
+    else:
+        form = AddForm()
+    return render(request, 'polls/add_post.html', {'form': form})
+
