b5f788a325c957f7eb98681a8f075db7322462e4
chris <chris.zhao@sonymobile.com>
2016-08-01 15:29:57 +0800

    update mail address to confirm if send successful

--- mail/sendemail_django.py
+++ mail/sendemail_django.py
@@ -6,14 +6,14 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 # from_addr = "SG-CNBJ-omv-tools-git-G@sonymobile.com"
 from_addr = "DL-CNBJ-FOTA-EMMA-Lab@sonymobile.com"
 # from_addr = "chris.zhao@sonymobile.com"
-to_addr = 'chris.zhao@sonymobile.com'
+to_addr = 'OMVFOTATest@sonymobile.com'
 # cc_list = ['SG-CNBJ-omv-tools-git-G@sonymobile.com']
 #cc_list = ['DL-CNBJ-FOTA-EMMA-Lab@sonymobile.com']
 #cc_list = ['DL-CNBJ-TCT_EMMA_Coordination@sonymobile.com']
-cc_list = ['DL-WW-TCT_CAV@sonymobile.com']
+cc_list = ['chris.zhao@sonymobile.com', ]
 #cc_list = ('DL-CNBJ-FOTA-EMMA-Lab@sonymobile.com',)
 
 
 email = EmailMessage('[Test]django email ', 'Hello, just confirm receive mail by DL, please ignore',
-                     from_email=from_addr, to=['chris.zhao@sonymobile.com'], cc=cc_list)
+                     from_email=from_addr, to=[to_addr], cc=cc_list)
 email.send()
